171ed7ec28d9cbb28da49d7b67e0cd44618be605
Biki Pramanik <bikib2004@gmail.com>
2025-05-05 23:18:52 +0530

    init

--- src/main/java/GrayScale.java
+++ src/main/java/GrayScale.java
@@ -1,24 +1,21 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class GrayScale {
 
-    public BufferedImage convertToGrayScaleManual(BufferedImage image){
+    public BufferedImage convertToGrayScaleManual(BufferedImage image) {
 
-        if(image == null){
+        if (image == null) {
             System.out.println("Image path null");
             return null;
         }
         int width = image.getWidth();
         int height = image.getHeight();
 
-        BufferedImage output = new BufferedImage(width,height,BufferedImage.TYPE_BYTE_GRAY);
-        for (int y = 0; y < height ; y++) {
-            for (int x = 0; x < width ; x++) {
-                int rgb = image.getRGB(x,y);
+        BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = image.getRGB(x, y);
 
                 int r = (rgb >> 16) & 0xff;
                 int g = (rgb >> 8) & 0xff;
@@ -35,6 +32,7 @@ public class GrayScale {
 
         return output;
     }
+
     public BufferedImage convertToGrayScale(BufferedImage image) {
         if (image == null) {
             System.out.println("Image null");
